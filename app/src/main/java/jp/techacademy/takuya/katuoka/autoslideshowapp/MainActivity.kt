8143f17f2731deb5271fe9ba5dc1ac9de1d80ad7
katsuoka_takuya <59katsuoka@gmail.com>
2019-04-04 16:37:49 +0900

    button

--- app/src/main/java/jp/techacademy/takuya/katuoka/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/takuya/katuoka/autoslideshowapp/MainActivity.kt
@@ -59,18 +59,20 @@ class MainActivity : AppCompatActivity() {
 
 
         buttonMove.setOnClickListener {
-            if (cursor.moveToFirst()) {
-                // indexからIDを取得し、そのIDから画像のURIを取得する
-                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-                val id = cursor.getLong(fieldIndex)
-                val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-
-                imageView.setImageURI(imageUri)
+            if (cursor.moveToNext()) {
+                cursor.moveToFirst()
             }
+            // indexからIDを取得し、そのIDから画像のURIを取得する
+            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+            val id = cursor.getLong(fieldIndex)
+            val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+            imageView.setImageURI(imageUri)
         }
-        cursor.close()
     }
 }
+
+
 
 
 
